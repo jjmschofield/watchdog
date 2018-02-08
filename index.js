61337ef51fa37fb301bd98526f200b2cb3e218dc
jjmschofield <jjmschofield@googlemail.com>
2018-02-08 22:41:26 +0000

    Reducing bootstrap bloat and updating dependencies

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const path = require('path');
+const express = require('express');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'public/views'));
+app.get('/', (req, res) => res.render('index'));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
