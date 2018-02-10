c49bdae038208ef58a9a7e8f594ab019961f9d49
Felisia Martini <felisia_martini@hotmail.com>
2018-02-10 17:27:10 +0000

    Modified command router to handle arguments in slack commands

--- commands/router.js
+++ commands/router.js
@@ -1,19 +1,47 @@
 const express = require('express');
 const {slackAuthentication} = require('./middleware/authenticate');
+const {COMMAND_TYPES} = require('./definitions/COMMAND_TYPES');
+const {getHelpText} = require('./handlers/help');
 
 module.exports = {
     createCommandRouter
 };
 
-function createCommandRouter(){
-  const router = express.Router();
+function createCommandRouter() {
+    const router = express.Router();
 
-  router.use(slackAuthentication);
+    router.use(slackAuthentication);
 
-  router.post('/', (req,res)=>{
-      console.log(req.body);
-     res.status(200).send('hi');
-  });
+    router.post('/', commandController);
 
-  return router;
-};
+    return router;
+}
+
+function commandController(req, res) {
+
+    console.log(req.body);
+
+
+    const args = req.body.text.split(' ');
+    const lowercaseArgs = args.map(arg => arg.toLowerCase());
+
+    const command = lowercaseArgs[0];
+
+    let handler;
+    switch (command) {
+        case COMMAND_TYPES.HELP :
+            handler = getHelpText;
+            break;
+        default :
+            handler = getHelpText;
+    }
+
+    handler()
+        .then((result) => {
+            res.status(200).send(result);
+        })
+        .catch((error) => {
+            res.status(error.status || 500).send(error.message);
+        });
+
+}
