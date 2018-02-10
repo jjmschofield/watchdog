9e91918724fe2e951c935fce8cf8275fcb364bfc
Felisia Martini <felisia_martini@hotmail.com>
2018-02-10 16:03:19 +0000

    Making sure that authentication fails when SLACK_COMMAND_TOKEN  env var is not set

--- commands/middleware/authenticate.js
+++ commands/middleware/authenticate.js
@@ -2,11 +2,17 @@ const {getConfig} = require('../../config/config');
 
 module.exports = {
     slackAuthentication: (req, res, next) => {
+        const slackSecret = getConfig().slackCommandToken;
+
+        if(typeof slackSecret !== 'string') {
+            res.status(503).send();
+            console.warn('SLACK_COMMAND_TOKEN not set. Sending 503.');
+            return;
+        }
+
         const payload = req.body;
 
-        console.log(payload);
-
-        if (payload && payload.token === getConfig().slackCommandToken) {
+        if (payload && payload.token === slackSecret) {
             next();
         }
         else {
