d85e789aff0c2b46edb931bf2b9813a4d4c2484f
jjmschofield <jjmschofield@googlemail.com>
2018-02-11 00:57:29 +0000

    Correctiong syntax error

--- src/commands/handlers/check.js
+++ src/commands/handlers/check.js
@@ -102,7 +102,7 @@ function getSlackFields(checks) {
 
 function isAllowedUrl(url){
     return typeof url === 'string'
-        && validator.isURL(url, )
+        && validator.isURL(url)
         && validator.isFQDN(url.replace('http://','').replace('https://',''))
         && !validator.contains(url, 'localhost')
         && !validator.contains(url, '127.0.0.1');
