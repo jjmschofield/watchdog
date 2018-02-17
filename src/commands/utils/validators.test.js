04d25ea6b14216a665c9f2ee5c3c0d57aeb9f1e3
Felisia Martini <felisia_martini@hotmail.com>
2018-02-17 18:38:20 +0000

    Added tests for validatior.js

--- src/commands/utils/validators.test.js
+++ src/commands/utils/validators.test.js
@@ -1,40 +1,81 @@
-describe('src/commands/utils/validators', () => {
-  describe('isAllowedUrl', () => {
-    describe('when given a valid URL', () => {
+let underTest;
+
+beforeEach(() => {
+// eslint-disable-next-line global-require
+  underTest = require('./validators');
+});
+
+describe('isAllowedUrl', () => {
+  describe('when given a valid URL', () => {
+    describe('and the url contains https', () => {
+      const validUrl = 'https://valid.url';
+
       it('should return true', () => {
-        expect(false).toEqual(true);
+        const result = underTest.isAllowedUrl(validUrl);
+        expect(result).toEqual(true);
       });
     });
+    describe('and the url contains http', () => {
+      const validUrl = 'http://valid.url';
 
-    describe('when given a non-string', () => {
-      it('should return false', () => {
-        expect(false).toEqual(true);
+      it('should return true', () => {
+        const result = underTest.isAllowedUrl(validUrl);
+        expect(result).toEqual(true);
       });
     });
+    describe('and the url contains neither https nor http', () => {
+      const validUrl = 'valid.url';
 
-
-    describe('when given a string which is not a valid URL pattern', () => {
-      it('should return false', () => {
-        expect(false).toEqual(true);
+      it('should return true', () => {
+        const result = underTest.isAllowedUrl(validUrl);
+        expect(result).toEqual(true);
       });
     });
+  });
 
-    describe('when given a URL that does not contain an FQDN', () => {
-      it('should return false', () => {
-        expect(false).toEqual(true);
-      });
+  describe('when given a non-string', () => {
+    const nonString = 123;
+
+    it('should return false', () => {
+      const result = underTest.isAllowedUrl(nonString);
+      expect(result).toEqual(false);
     });
+  });
 
-    describe('when given a URL that tries to target localhost', () => {
-      it('should return false', () => {
-        expect(false).toEqual(true);
-      });
+
+  describe('when given a string which is not a valid URL pattern', () => {
+    const notValidUrl = 'notvalidurl';
+
+    it('should return false', () => {
+      const result = underTest.isAllowedUrl(notValidUrl);
+      expect(result).toEqual(false);
     });
+  });
 
-    describe('when given a URL that tries to target 127.0.0.1', () => {
-      it('should return false', () => {
-        expect(false).toEqual(true);
-      });
+  describe('when given a URL that does not contain an FQDN', () => {
+    const notValidUrl = 'https://123.1.1.1';
+
+    it('should return false', () => {
+      const result = underTest.isAllowedUrl(notValidUrl);
+      expect(result).toEqual(false);
+    });
+  });
+
+  describe('when given a URL that tries to target localhost', () => {
+    const notValidUrl = 'https://localhost';
+
+    it('should return false', () => {
+      const result = underTest.isAllowedUrl(notValidUrl);
+      expect(result).toEqual(false);
+    });
+  });
+
+  describe('when given a URL that tries to target 127.0.0.1', () => {
+    const notValidUrl = 'https://127.0.0.1';
+
+    it('should return false', () => {
+      const result = underTest.isAllowedUrl(notValidUrl);
+      expect(result).toEqual(false);
     });
   });
 });
