8538864a0aeec19a5b23eeee7e223b8c88ac748e
Jack Schofield <jjmschofield@googlemail.com>
2018-02-18 23:56:26 +0000

    Adding tests structure for morgan

---
+++ src/logger/middleware/requestLoggers.test.js
@@ -0,0 +1,51 @@
+describe('logRequestStandard', () => {
+  describe('when the request status code is less then 500', () => {
+    it('should log to stdout', () => {
+      expect(true).toEqual(false);
+    });
+  });
+
+  describe('when the request status code is greater then or equal to 500', () => {
+    it('should not log', () => {
+      expect(true).toEqual(false);
+    });
+  });
+
+  describe('when running in production', () => {
+    it('should log using the combined format', () => {
+      expect(true).toEqual(false);
+    });
+  });
+
+  describe('or when not running in production', () => {
+    it('should log using the dev format', () => {
+      expect(true).toEqual(false);
+    });
+  });
+});
+
+describe('logRequestError', () => {
+  describe('when the request status code is less then 500', () => {
+    it('should not log', () => {
+      expect(true).toEqual(false);
+    });
+  });
+
+  describe('when the request status code is greater then or equal to 500', () => {
+    it('should log to stderr', () => {
+      expect(true).toEqual(false);
+    });
+  });
+
+  describe('when running in production', () => {
+    it('should log using the combined format', () => {
+      expect(true).toEqual(false);
+    });
+  });
+
+  describe('or when not running in production', () => {
+    it('should log using the dev format', () => {
+      expect(true).toEqual(false);
+    });
+  });
+});
